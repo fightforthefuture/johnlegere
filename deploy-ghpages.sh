d1708d6a59c163dc0b1e6f995715ce32b92b823b
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-01-07 20:16:11 -0600

    fixed title

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,7 +3,8 @@ cd dist || exit 0;
 
 if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" ]
   then
-    (git init
+    (cp ../CNAME ./CNAME
+     git init
      git config user.name "Travis-CI"
      git config user.email "travis@example.org"
      git add .
@@ -11,6 +12,3 @@ if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" ]
      git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
     )
 fi
-
-# Move this to L6 once there's a CNAME file
-# cp ../CNAME ./CNAME
